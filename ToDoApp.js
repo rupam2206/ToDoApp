f0528d6e2b4464e0bb2a53b4971ca60342d2d1fd
Rupam Biswas <rupambiswasofficial2206@gmail.com>
2025-03-16 13:55:47 +0530

    Add files via upload

---
+++ ToDoApp.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector('button');
+let ul = document.querySelector('ul');
+let input = document.querySelector('input');
+btn.addEventListener('click',function(){
+    let item = document.createElement('li');
+    item.innerText = input.value;
+    let delBtn = document.createElement('button');
+    delBtn.innerText = 'Delete';
+    delBtn.classList.add('delete');
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    input.value = ' ';
+});
+ul.addEventListener('click',function(event){
+   if (event.target.nodeName == 'BUTTON'){
+    let listItem = event.target.parentElement;
+    listItem.remove();
+   }
+});
+// let delBtns = document.querySelectorAll('.delete');
+// for (delBtn of delBtns){
+//     delBtn.addEventListener('click',function(){
+//         let par = this.parentElement;
+//         par.remove();
+//     });
+// }
